1fde6c0853aca5402e053f20b3fcf7ad5f0d3260
JJ Wanda <jjwanda@gmail.com>
2017-07-26 07:37:36 -0700

    Yay I'm doing work

--- craigslist_grab.py
+++ craigslist_grab.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+prefered_hoods = ["(mission district)"]
+
 def fetch_search_results():
-	base = "https://sfbay.craigslist.org/search/sfc/roo"
+	base = "https://sfbay.craigslist.org/search/sfc/apa"
 	resp = requests.get(base, timeout=3)
 	resp.raise_for_status()
 	return resp.content, resp.encoding
@@ -18,13 +20,13 @@ def extract_listings(parsed):
 def get_prices(listing):
 	prices = listing.find_all(class_="result-price")
 	if len(prices) > 0:
-		return prices[0].string
+		return int(prices[0].string[1:])
 	return 0
 
 def get_district(listing):
 	hood = listing.find_all(class_="result-hood")
 	if len(hood) > 0:
-		return hood[0].string
+		return hood[0].string[1:]
 	return "N/A"
 
 def get_title(listing):
@@ -33,16 +35,56 @@ def get_title(listing):
 
 def get_link(listing):
 	link = listing.find_all("a", class_="result-title")
-	return link.
+	# return link.
+
+def get_beds(listing):
+	housing = listing.find_all(class_="housing")
+	print(housing)
+	if len(housing) > 0:
+		return housing[0].string
+	return "N/A"
+
+def filter_via_hood(listings):
+	filtered_list = []
+	for listing in listings:
+		if listing.district in prefered_hoods:
+			filtered_list.append(listing)
+	return filtered_list
+
+class Listing(object):
+
+	price = 0
+	district = ""
+	title = ""
+	link = ""
+	beds = ""
+	org_html = None
+
+	"""docstring for Listing"""
+	def __init__(self, listing_row):
+		super(Listing, self).__init__()
+		self.price = get_prices(listing_row)
+		self.district = get_district(listing_row)
+		self.title = get_title(listing_row)
+		self.link = get_title(listing_row)
+
+	def __str__(self):
+		return str(self.price) + " - " + str(self.district)
+	def __repr__(self):
+		return str(self)
+		
 	
 
 results = fetch_search_results()
 doc = parse_source(results[0], encoding=results[1])
 listings = extract_listings(doc)
-# print(len(listings))
-print(listings[0].prettify())
+all_listings = []
+
 for listing in listings:
-	print(get_prices(listing))
-	print(get_district(listing))
-	print(get_title(listing))
-	print(get_link(listing))
+	get_beds(listing)
+	classListing = Listing(listing)
+	all_listings.append(classListing)
+
+filtered_list = filter_via_hood(all_listings)
+for filt in filtered_list:
+	print(filt)
